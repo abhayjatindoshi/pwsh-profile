650139522b63941995f943c5c1654c901f625891
Abhay Jatin Doshi <ajd1996@hotmail.com>
2025-04-20 23:34:33 +0530

    Create my-profile.ps1

---
+++ my-profile.ps1
@@ -0,0 +1,87 @@
+$lastUpdate = Get-Date -Date "2000-01-01 00:00:00"
+$profileUrl = "https://raw.githubusercontent.com/abhayjatindoshi/finances/refs/heads/main/quries.sql"
+
+$customProfileName = "my-profile.ps1"
+$customProfilePath = "$(Split-Path -parent $profile)/$customProfileName"
+
+function Invoke-Update {
+    if (not (Test-Path -Path $profile)) {
+        New-Item -Path $profile -ItemType File -Force | Out-Null
+        Write-Host "Profile file created $profile"
+    }
+
+    $profileData = Get-Content -Path $profile
+    $hasCustomProfileInit=$false
+    foreach ($line in $profileData) {
+        if ($line -match ". $customProfilePath") {
+            $hasCustomProfileInit=$true
+            break
+        }
+    }
+
+    if (-not $hasCustomProfileInit) {
+        Add-Content -Path $profile -Value ". $customProfilePath"
+        Write-Host "The script has been added to your profile."
+        return
+    }
+    
+    $today = Get-Date
+    if ($lastUpdate.AddDays(2) -gt $today) {
+        Write-Host "Last update: $lastUpdate"
+        Write-Host "Skipping update check..."
+        return;
+    }
+
+    if (Test-Path -Path $customProfilePath) {
+        Remove-Item -Path $customProfilePath -Force
+        Write-Host "Removed old profile: $customProfilePath"
+    }
+
+    Invoke-WebRequest $profileUrl -OutFile $customProfilePath
+    "$lastUpdate = Get-Date -Date `"$(Get-Date)`" `n" + (Get-Content $profile | Select-Object -Skip 1) | Set-Content $profile
+
+    # Check if the script already exists in the profile file
+    if (-not (Select-String -Path $profile -Pattern ". $customProfilePath" -Quiet)) {
+        # Add the script to the profile
+        Add-Content -Path $profilePath -Value ". $customProfilePath"
+        Write-Host "The script has been added to your profile."
+    }
+    else {
+        Write-Host "The script is already in your profile."
+    }
+}
+
+Set-Alias -Name "Invoke-Update" -Value Invoke-Update
+
+$alias = @{
+    "ff"     = "cd D:\finances"
+    "edit"   = "open $profile"
+    "reload" = ". $profile"
+    ".."     = "cd .."
+    "..."    = "cd ../.."
+    "...."   = "cd ../../.."
+    "gct"    = "git commit"
+    "gph"    = "git push"
+    "gpl"    = "git pull"
+    "gch"    = "git checkout"
+    "gb"     = "git branch"
+    "gbr"    = "git branch -r"
+    "gst"    = "git status"
+    "open"   = "start"
+};
+
+function Invoke-MyAlias {
+    $callingAlias = $MyInvocation.InvocationName
+    if (!$alias.ContainsKey($callingAlias)) {
+        Write-Error "Alias not found.";
+        return;
+    }
+
+    $command = $alias[$callingAlias]
+    Invoke-Expression "$command $args" 
+}
+
+foreach ($key in $alias.Keys) {
+    Set-Alias -Name $key -Value Invoke-MyAlias
+}
+Write-Output "Personal profile loaded - Welcome $Env:UserName!"
